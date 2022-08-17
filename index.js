bb4392e70894807f5677be36b7019969677ee74e
ghostrider8683 <ghostrider8683@gmail.com>
2022-08-17 22:26:28 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+let btnEl = document.getElementById("btn")
+let firstpassEl = document.getElementById("first-pass")
+let secondpassEl = document.getElementById("second-pass")
+
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L",
+"M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c",
+"d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t",
+"u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", 
+"9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=",
+"{","[","}","]",",","|",":",";","<",">",".","?","/"];
+
+btnEl.addEventListener("click", function(){
+    console.log("hello")
+    renderToScreen(firstpassEl)
+    renderToScreen(secondpassEl)
+})
+
+function generatePass(){
+    let passGenerated=[] 
+    for(let i=0; i<15; i++){
+        passGenerated += characters[Math.floor(characters.length*Math.random())]
+    }
+    console.log(passGenerated)
+    return passGenerated;
+}
+
+function renderToScreen(whichCell){
+    whichCell.textContent = generatePass()
+}
+console.log("hello")
